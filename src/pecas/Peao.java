8014103e24f289191077c4e67b108432b3ae090a
dargon <dargon@sylvester>
2026-07-21 14:53:57 -0300

    Peao change(double square initial move, for better class encapsulation)

--- src/pecas/Peao.java
+++ src/pecas/Peao.java
@@ -4,8 +4,6 @@ import chessUtil.*;
 import estrategias.EstrategiaPeao;
 
 public class Peao extends Peca {
-	private boolean primeiroMovimento = true;
-
 	public Peao(Cor cor, Coordenada coordenada_inicial) {
 		super(cor, coordenada_inicial, new EstrategiaPeao());
 	}
@@ -13,17 +11,14 @@ public class Peao extends Peca {
 	@Override
 	public boolean processarMovimento(Movimento movimento, tabuleiro.Tabuleiro tabuleiro) {
 		boolean result = estrategiaMovimento.validarMovimento(movimento, tabuleiro);
-		if (result) {
-			if (this.primeiroMovimento)
-				this.primeiroMovimento = false;
+		if (result)
 			coordenada = movimento.destino();
-		}
 
 		return result;
 	}
 
 	public boolean podeMoverDuasCasas() {
-		return primeiroMovimento;
+		return (cor == Cor.BRANCO) ? (coordenada.getRank() == 2) : (coordenada.getRank() == 7);
 	}
 
 	public int getDirecao() {
